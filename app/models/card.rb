39ed458fa6ea756fd399fa5b28aa364b7274035f
Javier Baena <jbaena@gmail.com>
2011-06-21 15:52:07 +0200

    Cambios en el cacheo de imágenes

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,17 @@
 require "open-uri"
 
 class Card < ActiveRecord::Base
-  has_attached_file :pic
+  has_attached_file :pic, :url => '/system/:id/:file_name', :path => ':rails_root/public/system/:id/:file_name'
   
+  Paperclip.interpolates :file_name  do |attachment, style|
+     attachment.instance.file_name
+   end
+     
   def picture_from_url(url)
       self.pic = open(url)
+  end
+  
+  def file_name
+    "card_"+self.id.to_s 
   end
 end
